d17c6986bbcefb0b7e5e84d1515bb5888a38092c
Kabisha Rai <chamlingkabisha@gmail.com>
2026-05-30 18:21:25 +0545

    feat: add Amazon cart test cases for add-to-cart flow

---
+++ cart-tests.spec.js
@@ -0,0 +1,17 @@
+// Amazon Cart Test Suite
+// Tester: Kabisha Rai
+// Date: 2026-05-30
+
+describe('Amazon Cart - Add to Cart', () => {
+
+  it('should add a single item to the cart', () => {
+    // Steps: navigate to product page, click Add to Cart
+    // Expected: cart icon updates to show item count = 1
+  });
+
+  it('should not allow quantity to exceed stock limit', () => {
+    // Steps: attempt to set qty > available stock
+    // Expected: error message displayed
+  });
+
+});
